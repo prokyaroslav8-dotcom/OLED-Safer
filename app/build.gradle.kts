9eba1d8bb81005e71525e2fb1197650defcc234c
iduchamp <prokyaroslav8@gmail.com>
2026-06-20 16:53:40 +0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,6 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    
-    // ОБНОВИ ЭТУ СТРОКУ (версия 2024.05.00)
     implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     
     implementation("androidx.compose.ui:ui")
